0314e326898fde1c1b7f6b17798fcfa2013b8b49
itskulen <valenhad7@gmail.com>
2022-09-29 10:02:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.cupcake
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.cupcake.databinding.FragmentStartBinding
+import com.example.cupcake.model.OrderViewModel
+
+/**
+ * This is the first screen of the Cupcake app. The user can choose how many cupcakes to order.
+ */
+class StartFragment : Fragment() {
+
+
+    // Binding objek instance yang sesuai dengan layout fragment_start.xml. Properti ini bukan nol antara callback siklus hidup onCreateView() dan onDestroyView(), saat hierarki tampilan dilampirkan ke fragmen.
+    private var binding: FragmentStartBinding? = null
+
+
+    // menggunakan delegasi properti Kotlin 'by activityViewModels()' dari artefak fragmen-ktx
+    private val sharedViewModel: OrderViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding = FragmentStartBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.startFragment = this
+    }
+
+    /**
+     * Start an order with the desired quantity of cupcakes and navigate to the next screen.
+     */
+    fun orderCupcake(quantity: Int) {
+        // memperbarui model tampilan dengan kuantitas
+        sharedViewModel.setQuantity(quantity)
+
+        // Jika belum ada rasa yang diatur dalam model tampilan, pilih vanilla sebagai rasa default
+        if (sharedViewModel.hasNoFlavorSet()) {
+            sharedViewModel.setFlavor(getString(R.string.vanilla))
+        }
+
+        /// Navigasikan ke tujuan berikutnya untuk memilih rasa cupcakes
+        findNavController().navigate(R.id.action_startFragment_to_flavorFragment)
+    }
+
+    /**
+     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
+     * is being removed. As a result, clear out the binding object.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
